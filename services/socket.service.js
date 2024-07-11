3f7eae1a29d703fcee96699e88d618701fc04feb
Amit <fridamit@gmail.com>
2024-07-11 19:08:50 +0300

    Implement role sockets and functionality

--- services/socket.service.js
+++ services/socket.service.js
@@ -6,6 +6,9 @@ export const socketService = {
     setupSocketAPI
 }
 
+const mentors = {}
+console.log('mentors:', mentors)
+
 function setupSocketAPI(server) {
     gIo = new Server(server, {
         cors: {
@@ -16,9 +19,29 @@ function setupSocketAPI(server) {
     gIo.on('connection', (socket) => {
         console.log('A user connected')
 
-        socket.on('join', ({ blockId, role }) => {
-            socket.join(blockId)
-            console.log(`${role} joined code block ${blockId}`)
+        socket.on('join', ({ blockId }) => {
+            socket.join(blockId);
+            console.log(`User ${socket.id} joined block ${blockId}`);
+
+            // Check if the block already has a mentor
+            if (!mentors[blockId]) {
+                mentors[blockId] = socket.id
+                socket.emit('setRole', 'mentor')
+                console.log(`User ${socket.id} is assigned as the mentor for block ${blockId}`)
+            } else {
+                socket.emit('setRole', 'student')
+                console.log(`User ${socket.id} is assigned as a student for block ${blockId}`)
+            }
+        })
+
+        socket.on('leave', ({ blockId }) => {
+            socket.leave(blockId)
+
+            if (mentors[blockId] === socket.id) {
+                delete mentors[blockId]
+                console.log(`Mentor ${socket.id} left block ${blockId}`)
+                gIo.to(blockId).emit('mentorLeft')
+            }
         })
 
         socket.on('disconnect', () => {
